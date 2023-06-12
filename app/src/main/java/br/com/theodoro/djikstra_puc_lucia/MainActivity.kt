564c506d15dc9a730e1ce9da6dbf0efa00f66912
Justamand <justamandd@gmail.com>
2023-06-12 10:07:02 -0300

    tela feita

--- app/src/main/java/br/com/theodoro/djikstra_puc_lucia/MainActivity.kt
+++ app/src/main/java/br/com/theodoro/djikstra_puc_lucia/MainActivity.kt
@@ -2,10 +2,70 @@ package br.com.theodoro.djikstra_puc_lucia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import br.com.theodoro.djikstra_puc_lucia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var spinnerFrom : Spinner
+    private lateinit var spinnerTo : Spinner
+
+    var from = -1;
+    var to = -1;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        spinnerFrom = binding.from
+        spinnerTo = binding.to
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.predios_pucc,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerFrom.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.predios_pucc,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerTo.adapter = adapter
+        }
+
+        spinnerFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                from = position - 1
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Nenhum item selecionado
+            }
+        }
+
+        spinnerTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Spinner "to" foi alterado
+                println("TOOO")
+                to = position - 1
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Nenhum item selecionado
+            }
+        }
     }
 }
